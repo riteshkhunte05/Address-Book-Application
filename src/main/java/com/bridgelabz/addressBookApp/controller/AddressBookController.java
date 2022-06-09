3acdc6bb7f8374a9b456ae800326e558f8cad05f
riteshkhunte05 <riteshkhunte05@gmail.com>
2022-06-09 23:12:03 +0530

    Added: Create rest Controller to demonstrate various HTTP Request

--- src/main/java/com/bridgelabz/addressBookApp/controller/AddressBookController.java
+++ src/main/java/com/bridgelabz/addressBookApp/controller/AddressBookController.java
@@ -6,7 +6,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/addressbook")
 public class AddressBookController {
     @RequestMapping(value = {"", "/", "/get"})
     public ResponseEntity<String> getAddressbookData() {
--- src/main/java/com/bridgelabz/addressBookApp/dto/AddressBookDTO.java
+++ src/main/java/com/bridgelabz/addressBookApp/dto/AddressBookDTO.java
@@ -12,8 +12,6 @@ public class AddressBookDTO {
     @Override
     public String toString() {
         return "AddressbookDTO{" +
-                "name='" + name + '\'' +
-                ", phNumber=" + phNumber +
-                '}';
+                "name='" + name + '\'' + ", phNumber=" + phNumber + '}';
     }
 }
