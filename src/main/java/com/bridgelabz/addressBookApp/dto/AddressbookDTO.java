d6ddee95598fd6382af360bd554a3a3cad9b5637
riteshkhunte05 <riteshkhunte05@gmail.com>
2022-06-10 23:45:23 +0530

    Added: Remaining Properties to Add Book DTO And Model with Ensuring Validations on the Add Book DTO

--- src/main/java/com/bridgelabz/addressBookApp/dto/AddressbookDTO.java
+++ src/main/java/com/bridgelabz/addressBookApp/dto/AddressbookDTO.java
@@ -1,28 +1,40 @@
 package com.bridgelabz.addressBookApp.dto;
 
 import lombok.Data;
+import lombok.ToString;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 
 @Data
+@ToString
 public class AddressbookDTO {
 
-    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Addressbook name is Invalid")
-    @NotEmpty(message = "Addressbook name cannot be null")
-    public String name;
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "FirstName name is Invalid")
+    @NotEmpty(message = "FirstName  cannot be null")
+    public String firstName;
 
-    @Pattern(regexp = "\"^[0-9]{0,2}[-][0-9]{10}\"" ,message = "phonenumber is invalid")
-    public String phNumber;
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "LastName name is Invalid")
+    @NotEmpty(message = "LastName name cannot be null")
+    public String lastName;
 
-    public AddressbookDTO(String name, String phNumber) {
-        this.name = name;
-        this.phNumber = phNumber;
-    }
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "City name is Invalid")
+    @NotEmpty(message = "City name  cannot be null")
+    public String city;
 
-    @Override
-    public String toString() {
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "state name is Invalid")
+    @NotEmpty(message = "State name  cannot be null")
+    public String state;
 
-        return "AddressbookDTO{" + "name='" + name + '\'' + ", phNumber=" + phNumber + '}';
-    }
+    @Pattern(regexp = "^[0-9]{3}[\\\\s]*[0-9]{2,}$",message = "Zip name is invalid")
+    @NotEmpty(message = "Zip code must not be null")
+    public String zip;
+
+    @Pattern(regexp = "^[\\+]?[(]?[0-9]{3}[)]?[-\\s\\.]?[0-9]{3}[-\\s\\.]?[0-9]{4,6}$" ,message = "phonenumber is invalid")
+    @NotEmpty(message = "phone number is not empty")
+    public String phone;
+
+    @Pattern(regexp = "^[a-z0-9]+@[a-z]+\\.[a-z]{2,3}$", message = "email is invalid")
+    @NotEmpty(message = "email should not be empty")
+    public String email;
 }
