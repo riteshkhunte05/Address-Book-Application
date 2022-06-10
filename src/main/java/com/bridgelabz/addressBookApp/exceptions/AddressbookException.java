7a4fd00b74e60d27c816b36bac7c63f0b346dfd4
riteshkhunte05 <riteshkhunte05@gmail.com>
2022-06-10 13:07:18 +0530

    Added: Throws (Custom Exception) User friendly Errors in case Address Book Id is Not Found

---
+++ src/main/java/com/bridgelabz/addressBookApp/exceptions/AddressbookException.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.addressBookApp.exceptions;
+
+public class AddressbookException extends RuntimeException{
+    public AddressbookException(String message) {
+        super(message);
+    }
+}
